68c0628f684dba90c3b42420532bae00e128c002
fateh1989 <0046alla@gmail.com>
2026-09-22 22:37:58 +0200

    CE: set application title

--- lib/main.dart
+++ lib/main.dart
@@ -240,7 +240,7 @@ class XCNav extends StatelessWidget {
       locale: context.locale,
       navigatorObservers:
           settingsMgr.rumOptOut.value ? [] : [DatadogNavigationObserver(datadogSdk: DatadogSdk.instance)],
-      title: 'xcNav',
+      title: 'CE',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         useMaterial3: false,
