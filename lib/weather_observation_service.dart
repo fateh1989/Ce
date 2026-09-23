b2590abb7de7e037c810514f15020851342ba80c
fateh1989 <0046alla@gmail.com>
2026-09-23 18:19:48 +0200

    Fix missing FlutterMap bounds import

--- lib/weather_observation_service.dart
+++ lib/weather_observation_service.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 
 import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
